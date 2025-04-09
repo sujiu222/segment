2ea779b58ec221c9ec1c43d8fec82dfbfac4636e
sujiu <2994338151@qq.com>
2025-04-09 21:17:07 +0800

    设置 publicPath: /segment/

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base: "./",
+  publicPath: "/segment/",
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  build: {
+    outDir: "dist", // 打包输出目录
+    assetsDir: "assets", // 静态资源输出目录
+    minify: "terser", // 压缩代码
+    terserOptions: {
+      compress: {
+        drop_console: true, // 移除 console
+        drop_debugger: true, // 移除 debugger
+      },
+    },
+  },
+});
